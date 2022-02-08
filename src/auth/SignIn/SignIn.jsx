a2e1986ea0650643c5058382f93ca3555f373e89
spice2445 <artem.klymenk@gmail.com>
2022-02-08 22:24:38 +0200

    TeamDet

---
+++ src/auth/SignIn/SignIn.jsx
@@ -0,0 +1,109 @@
+
+import React from 'react';
+
+const SignIn = () => {
+  return (
+    <div>
+        <div id="loading">
+            <div id="loading-center"></div>
+        </div>
+
+        <div className="wrapper">
+            <section className="sign-in-page">
+                <div id="container-inside">
+                    <div id="circle-small"></div>
+                    <div id="circle-medium"></div>
+                    <div id="circle-large"></div>
+                    <div id="circle-xlarge"></div>
+                    <div id="circle-xxlarge"></div>
+                </div>
+                <div className="container p-0">
+                    <div className="row no-gutters">
+                        <div className="col-md-6 text-center pt-5">
+                            <div className="sign-in-detail text-white">
+                                <a className="sign-in-logo mb-5" href="sign-in.html#"><img
+                                        src="./assets/images/logo-full.png" className="img-fluid" alt="logo" /></a>
+                                <div className="sign-slider overflow-hidden">
+                                    <ul className="swiper-wrapper list-inline m-0 p-0">
+                                        <li className="swiper-slide">
+                                            <img src="./assets/images/login/1.png" className="img-fluid mb-4" alt="logo" />
+                                            <h4 className="mb-1 text-white">Find new friends</h4>
+                                            <p>
+                                                It is a long established fact that a reader will be
+                                                distracted by the readable content.
+                                            </p>
+                                        </li>
+                                        <li className="swiper-slide">
+                                            <img src="./assets/images/login/2.png" className="img-fluid mb-4" alt="logo" />
+                                            <h4 className="mb-1 text-white">Connect with the world</h4>
+                                            <p>
+                                                It is a long established fact that a reader will be
+                                                distracted by the readable content.
+                                            </p>
+                                        </li>
+                                        <li className="swiper-slide">
+                                            <img src="./assets/images/login/3.png" className="img-fluid mb-4" alt="logo" />
+                                            <h4 className="mb-1 text-white">Create new events</h4>
+                                            <p>
+                                                It is a long established fact that a reader will be
+                                                distracted by the readable content.
+                                            </p>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-md-6 bg-white pt-5 pt-5 pb-lg-0 pb-5">
+                            <div className="sign-in-from">
+                                <h1 className="mb-0">Sign in</h1>
+                                <p>
+                                    Enter your email address and password to access admin panel.
+                                </p>
+                                <form className="mt-4">
+                                    <div className="form-group">
+                                        <label className="form-label" for="exampleInputEmail1">Email address</label>
+                                        <input type="email" className="form-control mb-0" id="exampleInputEmail1"
+                                            placeholder="Enter email" />
+                                    </div>
+                                    <div className="form-group">
+                                        <label className="form-label" for="exampleInputPassword1">Password</label>
+                                        <a href="sign-in.html#" className="float-end">Forgot password?</a>
+                                        <input type="password" className="form-control mb-0" id="exampleInputPassword1"
+                                            placeholder="Password" />
+                                    </div>
+                                    <div className="d-inline-block w-100">
+                                        <div className="form-check d-inline-block mt-2 pt-1">
+                                            <input type="checkbox" className="form-check-input" id="customCheck11" />
+                                            <label className="form-check-label" for="customCheck11">Remember Me</label>
+                                        </div>
+                                        <button type="submit" className="btn btn-primary float-end">
+                                            Sign in
+                                        </button>
+                                    </div>
+                                    <div className="sign-info">
+                                        <span className="dark-color d-inline-block line-height-2">Don't have an account?
+                                            <a href="sign-up.html">Sign up</a></span>
+                                        <ul className="iq-social-media">
+                                            <li>
+                                                <a href="sign-in.html#"><i className="ri-facebook-box-line"></i></a>
+                                            </li>
+                                            <li>
+                                                <a href="sign-in.html#"><i className="ri-twitter-line"></i></a>
+                                            </li>
+                                            <li>
+                                                <a href="sign-in.html#"><i className="ri-instagram-line"></i></a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+    </div>    
+  );
+};
+
+export default SignIn;
